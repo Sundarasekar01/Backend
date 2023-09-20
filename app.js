02ec40d27da9e3145d0b6b203095703183d8837c
Sundarasekar T <sundarasekar01@gmail.com>
2023-09-20 15:45:04 +0530

    main file updated

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const express = require("express")
-const app = express()
-const path=require('path')
+// app.js
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose'); // Import Mongoose
 
 
-// app.set('view engine','ejs')
-app.use(express.static(path.join(__dirname, 'public')));
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Configure the database connection
+mongoose.connect('mongodb://localhost/kibs', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Handle database connection events
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+// Import route modules
+const authRoutes = require('./routes/authRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+
+// Use route modules
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/teacher', teacherRoutes);
+app.use('/student', studentRoutes);
+
+
+
+// Start the server...
 
 
 app.listen(4000,()=>{
