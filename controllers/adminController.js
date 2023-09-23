fb87de98a854d90372e304304a61f7d9bf68eaf2
Sundarasekar T <sundarasekar01@gmail.com>
2023-09-23 21:27:36 +0530

    all the files are deleted

--- controllers/adminController.js
+++
@@ -1,5 +0,0 @@
-// controllers/adminController.js
-const User = require('../models/User');
-const Subject = require('../models/Subject');
-
-// Implement admin actions (e.g., CRUD operations on users and subjects)
